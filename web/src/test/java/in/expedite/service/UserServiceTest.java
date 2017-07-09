831b8a0e0a909268681bbe51813cce12476925e6
vijaykarthik <vijaykarthik@DESKTOP-GVG3AE4>
2017-07-09 18:47:21 +0530

    corrected User issues

--- web/src/test/java/in/expedite/service/UserServiceTest.java
+++ web/src/test/java/in/expedite/service/UserServiceTest.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
+import in.expedite.entity.State;
+import in.expedite.entity.User;
+
+@Ignore 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTest {
@@ -19,11 +22,24 @@ public class UserServiceTest {
 	@Autowired
 	UserService userService;
 	
-	@Test
+	
 	public void searchUser(){
 		userService.findUserByName("vijay").forEach(usr->{
 			log.info(usr.toString());
 		});
 		
+	}
+	
+	@Test
+	public void addUser() {
+		for(int i=1;i<=30;i++){
+			User user = new User();
+			user.setEmail("Team.member."+i+"@team.com");
+			user.setFirstName("Team");
+			user.setUserId("teamMember"+i);
+			user.setSecondName("Member"+i);
+			user.setState(State.ACTIVE.getState());
+			userService.addUser(user);
+		}
 	}
 }
